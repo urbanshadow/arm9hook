0fcdb73415b9b5f1b9e3ed5f7518993f3d658cc8
LuisCondes <luis.condesdiaz@gmail.com>
2016-02-29 17:58:27 +0100

    Remove objects

--- build/bs-start.d
+++
@@ -1,2 +0,0 @@
-bs-start.o: \
- c:/Users/Urbanshadow/Downloads/arm9hook/source/abstraction/bs-start.s
--- build/hid.d
+++
@@ -1,7 +0,0 @@
-hid.o: c:/Users/Urbanshadow/Downloads/arm9hook/source/hid.c \
- c:/Users/Urbanshadow/Downloads/arm9hook/source/hid.h \
- c:/Users/Urbanshadow/Downloads/arm9hook/source/common.h
-
-c:/Users/Urbanshadow/Downloads/arm9hook/source/hid.h:
-
-c:/Users/Urbanshadow/Downloads/arm9hook/source/common.h:
