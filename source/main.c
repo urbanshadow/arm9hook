4e49aa03d5ac76cfa6794b8ea79e038e7d3a80a5
urbanshadow <luis.condesdiaz@gmail.com>
2016-02-29 20:17:19 +0100

    Reduced binary size

--- source/main.c
+++ source/main.c
@@ -1,24 +1,12 @@
-#include "types.h"
-#include <stdio.h>
-#include <string.h>
-
-/*
-Caller
-Result ret = 0;
-u32 *cmdbuf = getThreadCommandBuffer();
-cmdbuf[0] = IPC_MakeHeader(0x6A,0,2); // 0x6A0010?
-cmdbuf[1] = IPC_Desc_PXIBuffer(8, 0, 0);
-cmdbuf[2] = payloadfile;
-
-if(R_FAILED(ret = svcSendSyncRequest(ampxiHandle)))return ret;
-
-return (Result)cmdbuf[1];
-*/
-//Callee
-__asm__("STMFD SP!,{R0-R11,LR}\n\t"
-			  "MOV R0,R4");
-void arm9hook(u32* cmdbuffer){ 
-	memcpy((u32*)cmdbuffer[2],((u32*)0x01FF8000),(cmdbuffer[1] & ~0xFFu) >> 8);
+typedef unsigned int u32;
+__asm__("STMFD SP!,{R0-R11,LR}");
+__asm__("MOV R0,R4");
+__attribute__((naked)) void pyldcpy(u32 *s1,const u32 *s2,u32 size){
+	int i;
+	for(i=0;i<(size/4);i++) *(s1+i) = *(s2+i);
+}
+__attribute__((naked)) void arm9hook(u32* cmdbuffer){ 
+	pyldcpy(((u32*)0x01FF8000),(u32*)cmdbuffer[2],(cmdbuffer[1] & ~0xFFu) >> 8);
 	((void (*)())0x01FF8000)();
 }
 __asm__("LDMFD SP!,{R0-R11,LR}");
