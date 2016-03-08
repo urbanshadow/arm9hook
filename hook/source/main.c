3b215c34a68287fc4da6219247a1e0fde77d15c5
urbanshadow <urbanshado08@gmail.com>
2016-03-08 21:24:07 +0100

    Modified arm9 payload

--- hook/source/main.c
+++ hook/source/main.c
@@ -14,34 +14,18 @@ void __attribute__((section (".text.start"),naked)) _start(void){
 	".word 0xDEADBEEF"); // This will be patched afterwards to firm dependant return position.
 }
 
-void __attribute__((naked)) printcolor(unsigned char b,unsigned char g,unsigned char r) {
-	unsigned int i;
-	unsigned char *bottom = (unsigned char *)0x2008CA00;
-	for(i=0;i<460800;i=i+3) {
-		*(bottom+i) = b;
-		*(bottom+i+1) = g;
-		*(bottom+i+2) = r;
-	}
-}
-
 void __attribute__((naked)) arm9hook(unsigned int* cbuff){
 	__asm__("STMFD SP!,{LR}");
 	unsigned int i,paysize,*payload,*itcm;
 	
-	printcolor(0,0,255);
-	
 	paysize = ((cbuff[1] & ~0xFFu) >> 8);
 	itcm = (unsigned int*)0x01FF8000; //Start of unused itcm region
-	payload = (unsigned int*)cbuff[2]; //payload pointer from the command header
-	
-	printcolor(255,0,0);
+	payload = *((unsigned int**)cbuff[2]); //payload pointer from the command header
 	
 	// Memcpy wannabe
 	for(i=0;i<(paysize/4);i++) {
 		*(itcm+i) = *(payload+i);
 	}
-	
-	printcolor(0,255,0);
 	
 	// Jump to payload after copy
 	((void (*)())0x01FF8000)();
--- payload/source/main.c
+++ payload/source/main.c
@@ -1,20 +1,17 @@
 #include "hid.h"
 //		bottom1   0x2008CA00-0x200C4E00
 //		bottom2   0x200C4E00-0x200FD200
-void printcolor(unsigned char b,unsigned char g,unsigned char r) {
-	unsigned int i;
-	unsigned char *bottom = (unsigned char *)0x2008CA00;
-	for(i=0;i<460800;i=i+3) {
-		*(bottom+i) = b;
-		*(bottom+i+1) = g;
-		*(bottom+i+2) = r;
-	}
-}
 
 void __attribute__((section (".text.start"),naked)) _start()
 {
 	__asm__("STMFD SP!,{LR}");
-	printcolor(255,255,255);
+	unsigned int i;
+	unsigned char *bottom = (unsigned char *)0x2008CA00;
+	for(i=0;i<460800;i=i+3) {
+		*(bottom+i) = 255;
+		*(bottom+i+1) = 255;
+		*(bottom+i+2) = 255;
+	}
     __asm__("LDMFD SP!,{PC}");
 }
 
